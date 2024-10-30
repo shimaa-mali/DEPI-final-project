879a9bab2cb50b9d5b47787b48c650201e881a27
AlShimaa <shimaa.mali19@gmail.com>
2024-10-30 20:09:33 +0300

    upload file

--- src/app/componants/blog/blog.component.ts
+++ src/app/componants/blog/blog.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  photos = [
+    './assets/ph1.png',
+    './assets/ph3.png',
+    './assets/ph4.png',
+    './assets/ph2.jpg',
+    './assets/ph5.png',
+    './assets/ph6.jpg',
+    './assets/ph1.png',
+    './assets/ph3.png',
+    './assets/ph4.png',
+    './assets/ph2.jpg',
+    './assets/ph5.png',
+    './assets/ph6.jpg',
+
+  ];
+  currentIndex = 0;
+
+  prev() {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.photos.length - 1;
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.photos.length;
+  }
 
 }
