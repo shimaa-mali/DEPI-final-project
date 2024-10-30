3521c7943b181f8c67a5fda1c6e10f148d3851ad
AlShimaa <shimaa.mali19@gmail.com>
2024-10-30 10:15:21 +0300

    new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componants/home/home.component';
+import { NavbarComponent } from './componants/navbar/navbar.component';
+import { FooterComponent } from './componants/footer/footer.component';
+import { AboutComponent } from './componants/about/about.component';
+import { ProjectsComponent } from './componants/projects/projects.component';
+import { DetailProjectComponent } from './componants/detail-project/detail-project.component';
+import { BlogComponent } from './componants/blog/blog.component';
+import { ContactComponent } from './componants/contact/contact.component';
+import { NotFoundComponent } from './componants/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent,
+    DetailProjectComponent,
+    BlogComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
